962a9fc819d06b1241b4b6927d6fc86305fd43f4
ASAKURA Kazuki <32762324+Arthur1@users.noreply.github.com>
2025-10-30 12:20:48 +0900

    use --now option

--- distributions/otelcol-mackerel/postinstall.sh
+++ distributions/otelcol-mackerel/postinstall.sh
@@ -4,8 +4,7 @@ if command -v systemctl >/dev/null 2>&1; then
   if [ -d /run/systemd/system ]; then
     systemctl daemon-reload
   fi
-  systemctl enable otelcol-mackerel.service
   if [ -d /run/systemd/system ]; then
-    systemctl restart otelcol-mackerel.service
+    systemctl enable --now otelcol-mackerel.service
   fi
 fi
--- distributions/otelcol-mackerel/preremove.sh
+++ distributions/otelcol-mackerel/preremove.sh
@@ -2,7 +2,6 @@
 
 if [ "$1" != "1" ]; then
   if command -v systemctl >/dev/null 2>&1; then
-    systemctl stop otelcol-mackerel.service
-    systemctl disable otelcol-mackerel.service
+    systemctl disable --now otelcol-mackerel.service
   fi
 fi
