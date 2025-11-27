9145cc1288934a8eb938ae3e74e87481d6d6478d
lufia <lufia@lufia.org>
2025-11-27 15:17:05 +0900

    feat: read API_KEY from systemd-creds

--- exporter/mackerelotlpexporter/config_test.go
+++ exporter/mackerelotlpexporter/config_test.go
@@ -1,9 +1,12 @@
 package mackerelotlpexporter
 
 import (
+	"os"
+	"path/filepath"
 	"testing"
 
 	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
 )
 
 func TestConfig_Validate(t *testing.T) {
@@ -27,6 +30,16 @@ func TestConfig_Validate(t *testing.T) {
 		cfg := &Config{}
 		t.Setenv("MACKEREL_APIKEY", "dummyapikey")
 		t.Setenv("MACKEREL_API_KEY", "dummyapikey")
+		assert.NoError(t, cfg.Validate())
+	})
+	t.Run("valid if Mackerel API key is provided via systemd-creds", func(t *testing.T) {
+		cfg := &Config{}
+		dir := t.TempDir()
+		t.Setenv("CREDENTIALS_DIRECTORY", dir)
+		file := filepath.Join(dir, "mackerel-apikey")
+		err := os.WriteFile(file, []byte("dummyapikey"), 0600)
+		require.NoError(t, err)
+
 		assert.NoError(t, cfg.Validate())
 	})
 	t.Run("invalid if no Mackerel API keys are provided", func(t *testing.T) {
