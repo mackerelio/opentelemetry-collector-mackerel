2950d41009dce2e84282168a6089232bdbd5779b
ASAKURA Kazuki <32762324+Arthur1@users.noreply.github.com>
2025-12-18 10:39:42 +0900

    add test assertions for default config

--- exporter/mackerelotlpexporter/factory_test.go
+++ exporter/mackerelotlpexporter/factory_test.go
@@ -6,6 +6,7 @@ import (
 
 	"github.com/stretchr/testify/assert"
 	"go.opentelemetry.io/collector/component/componenttest"
+	"go.opentelemetry.io/collector/exporter/exporterhelper"
 )
 
 func TestCreateDefaultConfig(t *testing.T) {
@@ -14,4 +15,8 @@ func TestCreateDefaultConfig(t *testing.T) {
 
 	cfg := defaultConfig.(*Config)
 	assert.Equal(t, 10*time.Second, cfg.TimeoutConfig.Timeout)
+	queueCfg := cfg.QueueConfig.GetOrInsertDefault()
+	batchCfg := queueCfg.Batch.GetOrInsertDefault()
+	assert.Equal(t, exporterhelper.RequestSizerTypeBytes, batchCfg.Sizer)
+	assert.Equal(t, int64(5_000_000), batchCfg.MaxSize)
 }
