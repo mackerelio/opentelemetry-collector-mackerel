63858d0be9b25720786d4e0d1e4873c42e68770a
ASAKURA Kazuki <32762324+Arthur1@users.noreply.github.com>
2025-12-18 14:26:26 +0900

    change QueueConfig to optional type for otlpexporter v0.142.0

--- exporter/mackerelotlpexporter/config.go
+++ exporter/mackerelotlpexporter/config.go
@@ -5,15 +5,16 @@ import (
 	"os"
 
 	"go.opentelemetry.io/collector/config/configopaque"
+	"go.opentelemetry.io/collector/config/configoptional"
 	"go.opentelemetry.io/collector/config/configretry"
 	"go.opentelemetry.io/collector/exporter/exporterhelper"
 )
 
 type Config struct {
-	TimeoutConfig  exporterhelper.TimeoutConfig    `mapstructure:",squash"`
-	QueueConfig    exporterhelper.QueueBatchConfig `mapstructure:"sending_queue"`
-	RetryConfig    configretry.BackOffConfig       `mapstructure:"retry_on_failure"`
-	MackerelApiKey configopaque.String             `mapstructure:"mackerel_api_key"`
+	TimeoutConfig  exporterhelper.TimeoutConfig                             `mapstructure:",squash"`
+	QueueConfig    configoptional.Optional[exporterhelper.QueueBatchConfig] `mapstructure:"sending_queue"`
+	RetryConfig    configretry.BackOffConfig                                `mapstructure:"retry_on_failure"`
+	MackerelApiKey configopaque.String                                      `mapstructure:"mackerel_api_key"`
 	// MetricsEndpoint configurations are provided for testing purposes and may be modified or deprecated.
 	MetricsEndpoint string `mapstructure:"metrics_endpoint"`
 	// TracesEndpoint configurations are provided for testing purposes and may be modified or deprecated.
